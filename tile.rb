bb7eb850df2da6e81c3ca36d2b5eb9a14fee4958
davidFox <djfoxy11@gmail.com>
2020-09-24 09:59:54 -0500

    Start of tile class

--- tile.rb
+++ tile.rb
@@ -1,2 +1,25 @@
 # tile.rb
 
+class Tile
+    def initialize(symbol)
+        @symbol = symbol
+        @revealed = false
+        @flagged = false
+        @bomb = false
+    end
+
+    def reveal
+        if !@revealed
+            @revealed = true
+        else
+            puts "Tile already revealed"
+        end
+    end
+
+    def flag
+        @flagged ? @flagged = false : @flagged = true
+    end
+
+    
+
+end
