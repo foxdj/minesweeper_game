8ca7c4c146ac62238eab0ecf1f4f6e663bbbda59
davidFox <djfoxy11@gmail.com>
2020-09-24 10:09:27 -0500

    Tile.rb: added reader methods, adjacent_bomb_count, and reveal methods

--- tile.rb
+++ tile.rb
@@ -1,25 +1,54 @@
 # tile.rb
 
 class Tile
-    def initialize(symbol)
-        @symbol = symbol
-        @revealed = false
-        @flagged = false
-        @bomb = false
+  DELTAS = [
+    [-1, -1],
+    [-1,  0],
+    [-1,  1],
+    [ 0, -1],
+    [ 0,  1],
+    [ 1, -1],
+    [ 1,  0],
+    [ 1,  1]
+  ].freeze
+
+  attr_reader :pos
+
+    def initialize(board, pos)
+        @board, @pos = board, pos
+        @revealed, @flagged, @bombed = false, false, false
+    end
+
+    def bombed?
+        @bombed
+    end
+
+    def revealed?
+        @revealed
+    end
+
+    def flagged?
+        @flagged
+    end
+
+    def adjacent_bomb_count
+        neighbors.select(&:bombed?).count
     end
 
     def reveal
-        if !@revealed
-            @revealed = true
-        else
-            puts "Tile already revealed"
+        return self if flagged? || revealed?
+        @revealed = true
+
+        if !bombed? && adjacent_bomb_count == 0
+            neighbors.each(&:explore)
         end
+        self
     end
 
-    def flag
-        @flagged ? @flagged = false : @flagged = true
-    end
 
-    
+
+    def toggle_flag
+        @flagged = !@flagged unless @revealed
+    end
 
 end
